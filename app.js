65f905b77374644d4126882148ceb5bf82c6a527
EnterKey <enterkey88@gmail.com>
2014-03-16 16:35:31 +0900

    duplication part merge

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var path = require('path');
 var ejs = require('ejs');
 var socket = require('./socket/socket');
 
+/*
+ * write enterkey
+ */
+var user = require('./routes/user.js')
+var mainDB = require('./database/main.js')
+
+
 //var app = express();
 
 var app = require('express')(), server = require('http').createServer(app);
@@ -26,6 +33,15 @@ app.engine('.html', ejs.__express);
 ejs.open = '<?';
 ejs.close = '?>';
 
+
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('frontfloor'));
+app.use(express.session());
+app.use(function(req, res, next){
+	res.locals.session = req.session;
+	next();
+});
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -45,6 +61,11 @@ app.get('/users', user.list);
 
 app.get('/pc', routes.pc);
 app.get('/rc/rc', routes.rc);
+
+
+// 로그인
+app.post('/user/login', mainDB.login, user.login);
+app.get('/user/logout', user.logout);
 
 server.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
--- routes/user.js
+++ routes/user.js
@@ -6,3 +6,22 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+//로그인
+exports.login = function(req, res) {
+	var result = false;
+	if (res.locals.userInfo) {
+		registSession(req, res);
+		result = true;
+	}
+	res.send(result);
+}
+
+function registSession(req, res) {
+	req.session.userInfo = res.locals.userInfo;
+}
+
+exports.logout = function(req, res) {
+	clearSession(req);
+	res.redirect('/');
+}
