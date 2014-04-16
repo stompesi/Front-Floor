b6a46a56017cf579540846cc0fbcb9fbeff8fa9f
stompesi <stompesi@gmail.com>
2014-04-16 22:03:38 +0900

    init repository

--- server/common/md5.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author stompesi
- */
-exports.md5 = function(key) {
-	var crypto = require('crypto'),
-		md5hash = crypto.createHash('md5');
-	md5hash.update(key);
-	return md5hash.digest('hex');
-};
